09ecedf66c84b6b342a2dc7fc76fcf4f5d95357b
Sahil Kumar <sahilkumar952558@gmail.com>
2025-04-05 22:52:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/service", serviceRoutes);
